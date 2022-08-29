6cf7948348baddbec6d739f4b7d7055022b595fa
Ziyodbek Ahmadjonov <mr.prgrmr03@gmail.com>
2022-08-29 18:05:10 +0500

    first commit

---
+++ pages/_home/FirstSection/FirstSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../../../styles/Home.module.css'
+
+function FirstSection() {
+    return (
+        <div className={styles.container}>
+            First Section
+        </div>
+    );
+}
+
+export default FirstSection;
---
+++ pages/_home/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FirstSection from "./FirstSection/FirstSection";
+
+function HomePage() {
+    return (<div>
+        <FirstSection/>
+    </div>);
+}
+
+export default HomePage;
