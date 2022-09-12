36369ef8e0756020e546079555635f2d91d28654
Ziyodbek Ahmadjonov <mr.prgrmr03@gmail.com>
2022-09-12 17:42:14 +0500

    some

--- pages/products/index.js
+++ pages/products/index.js
@@ -15,6 +15,8 @@ import SecondSection from "./SecondSection";
 function Products() {
     const router = useRouter();
     const [open, setOpen] = useState(false);
+    const [hh, setHh] = useState(0);
+    const [white, setWhite] = useState(true);
 
     const toggle = () => {
         setOpen(!open);
@@ -25,17 +27,30 @@ function Products() {
         AOS.refresh()
     }, []);
 
-    return (<>
+    useEffect(() => {
+        const handleScroll = event => {
+            console.log('window.scrollY', window.scrollY);
+            if(window.scrollY >= 100) {
+                setWhite(false);
+            } else {
+                setWhite(true)
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+    }, [hh]);
+
+    return (<div>
         <div className={"some-bg"}>
-        <div className={"md:px-[1.6rem] fixed w-full bg-transparent top-0 z-50"} data-aos={"fade-up"}
+        <div className={white ? "md:px-[1.6rem] fixed w-full bg-transparent top-0 z-50":"md:px-[1.6rem] fixed w-full bg-white/90 backdrop-blur-xl top-0 z-50"} data-aos={"fade-up"}
             data-aos-duration={"1000"}>
             <div className={"flex justify-between items-center mx-auto container"}>
                 <Link href={"/"}>
                     <div className={"w-2/5"}>
-                        <Image src={LogoWhite} alt={'Logo'} height={"88"}/>
+                        <Image src={white ? LogoWhite : Logo} alt={'Logo'} height={"88"}/>
                     </div>
                 </Link>
-                <div className={"w-3/5 text-white"}>
+                <div className={white ? "w-3/5 text-white" : "w-3/5"}>
                     <ul className={!open ? "p-0 md:flex hidden m-0 justify-end" : "md:static absolute top-full w-full md:backdrop-blur-0 backdrop-blur-2xl md:bg-transparent bg-white/100 left-0 md:gap-0 gap-8 flex md:flex-row flex-col py-5 md:flex m-0 justify-end"}>
                         <li onClick={() => setOpen(false)}
                             className={router.asPath === '/' ? 'font-bold mx-5' : 'mx-5'}>
@@ -71,7 +86,7 @@ function Products() {
         </div>
         <SecondSection/>
         <FirstSection/>
-    </>);
+    </div>);
 }
 
 export default Products;
