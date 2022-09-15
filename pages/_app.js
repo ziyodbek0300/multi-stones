0f936fcfefeb5ec3b46c362472188538838e852e
AbrorKh <abrorkhayitbaev@gmail.com>
2022-09-15 19:51:20 -0400

    contents included

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,43 @@
 import '../styles/globals.css'
-import Layout from '../components/Layout';
 
-function MyApp({Component, pageProps}) {
-    return (<Layout>
+const MyApp = ({ Component, pageProps }) => {
+    const router = useRouter()
+    useEffect(() => {
+      const handleRouteChange = (url) => {
+        gtag.pageview(url)
+      }
+      router.events.on('routeChangeComplete', handleRouteChange)
+      router.events.on('hashChangeComplete', handleRouteChange)
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange)
+        router.events.off('hashChangeComplete', handleRouteChange)
+      }
+    }, [router.events])
+  
+    return (
+      <>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag.GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
         <Component {...pageProps} />
-    </Layout>)
-}
+      </>
+    )
+  }
 
 export default MyApp
