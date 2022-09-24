3daf295d62b99b7953b93355ec33f43e693bd32b
AbrorKh <abrorkhayitbaev@gmail.com>
2022-09-24 17:34:34 -0400

    main and products

--- pages/_home/SecondSection/SecondSection.jsx
+++ pages/_home/SecondSection/SecondSection.jsx
@@ -38,7 +38,7 @@ function SecondSection() {
                 <div data-aos="fade-up" data-aos-duration={"800"} className={"md:block relative hidden pb-0 mb-0 h-96 exBg"}>
                     <div className={"bg-tanerofka s opacity-0 absolute h-1/2 bottom-0 w-full z-40"}></div>
                     <div className={"absolute s opacity-0 z-40 bottom-0 p-6"}>
-                        <h3 className={"text-white text-24-30-600 mb-2"}>Grey quartz</h3>
+                        <h3 className={"text-white text-24-30-600 mb-2"}>Soapstone</h3>
                     </div>
                 </div>
                 <div data-aos="fade-up" data-aos-duration={"900"} className={"md:block relative hidden pb-0 mb-0 h-96 exBg"}>
