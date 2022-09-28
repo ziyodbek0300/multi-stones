5103a76b2978603664d3223b6345db19c0c95430
AbrorKh <abrorkhayitbaev@gmail.com>
2022-09-28 17:35:15 -0400

    ads code

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,10 @@ import '../styles/globals.css'
 import Layout from '../components/Layout';
 import Script from 'next/script'
 import * as gtag from '../lib/gtag'
-
+import Head from 'next/head'
 function MyApp({Component, pageProps}) {
     return (<Layout>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        {/* Global Site Tag (gtag.js) - Google Ads */}
         <Script
           strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
@@ -40,6 +40,51 @@ function MyApp({Component, pageProps}) {
             `,
           }}
         />
+
+          {/* <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            gtag('config', '${gtag.GA_TRACKING_ID}/dNeFCK_q_OADELu4g_Uo', {
+              'phone_conversion_number': '(332) 322-2244'
+            });
+            `,
+          }}
+        />
+
+        <Script
+                  id="gtag-init"
+                  strategy="afterInteractive"
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                    gtag('config', '${gtag.GA_TRACKING_ID}/dNeFCK_q_OADELu4g_Uo', {
+                      'phone_conversion_number': '(332) 322-2244'
+                    });
+                    `,
+                  }}
+                />
+
+        <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            function gtag_report_conversion(url) {
+              var callback = function () {
+                if (typeof(url) != 'undefined') {
+                  window.location = url;
+                }
+              };
+              gtag('event', 'conversion', {
+                  'send_to': '${gtag.GA_TRACKING_ID}/QaAkCMvJsuEDELu4g_Uo',
+                  'event_callback': callback
+              });
+              return false;
+            }`,
+            }}
+        /> */}
+
         <Component {...pageProps} />
     </Layout>)
 }
--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,33 @@ export default function Home() {
     return (
         <div>
             <Head>
-                <title>Multi Stones</title>
+                <title>Multi Stone Masters</title>
                 <meta name="description" content="Multi stone masters"/>
                 <link rel="icon" href="/favicon.ico"/>
                 <link rel="icon" href="/favicon.ico"/>
+                <script>
+                    {`
+                        function gtag_report_conversion(url) {
+                            var callback = function () {
+                              if (typeof(url) != 'undefined') {
+                                window.location = url;
+                              }
+                            };
+                            gtag('event', 'conversion', {
+                                'send_to': 'AW-10982841403/QaAkCMvJsuEDELu4g_Uo',
+                                'event_callback': callback
+                            });
+                            return false;
+                          }
+                    `}
+                </script>
+                <script>
+                    {`
+                        gtag('config', 'AW-10982841403/dNeFCK_q_OADELu4g_Uo', {
+                            'phone_conversion_number': '(332) 322-2244'
+                          });
+                    `}
+                </script>
             </Head>
 
             <main className={styles.main}>
