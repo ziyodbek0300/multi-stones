a8cf15f50166ab9157175d8ca0ef73ce48a21fc7
AbrorKh <abrorkhayitbaev@gmail.com>
2022-09-28 11:38:33 -0400

    google analytics included

--- pages/_app.js
+++ pages/_app.js
@@ -22,6 +22,24 @@ function MyApp({Component, pageProps}) {
             `,
           }}
         />
+
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-3EX12L863R`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || []; 
+              function gtag(){dataLayer.push(arguments);} 
+              gtag('js', new Date()); 
+              gtag('config', 'G-3EX12L863R');
+            `,
+          }}
+        />
         <Component {...pageProps} />
     </Layout>)
 }
