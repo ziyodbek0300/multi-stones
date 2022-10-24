83956ba2b667e67eddc62e0d7c68e8a98741ad4e
AbrorKh <abrorkhayitbaev@gmail.com>
2022-10-24 16:25:15 -0400

    hotjar included

--- pages/_app.js
+++ pages/_app.js
@@ -99,6 +99,24 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             `,
           }}
         />
+
+
+<Script
+          id="service-bell-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3216498,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        />
         <Component {...pageProps} />
     </Layout>)
 }
